efe0743339b410cf04958ddf27b6b10ce4028056
zzh <964108463@qq.com>
2020-02-27 17:54:59 +0800

    add retry

--- get_data.py
+++ get_data.py
@@ -66,42 +66,49 @@ for key in country2urls.keys():
     urls = country2urls[key]
     names = []
     for url in urls:
-        try:
-            options = webdriver.ChromeOptions()
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            options.add_argument('--no-sandbox')
-            desired_capabilities = DesiredCapabilities.CHROME
-            desired_capabilities['pageLoadStrategy'] = 'none'
-            driver = webdriver.Chrome(options=options)
-            driver.get(url)
-            name = (By.CSS_SELECTOR, '.float_lang_base_1.inlineblock')
-            name = WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located(name))
-            namestr = name.text.replace(' ', '_').replace('/', '_')
-            names.append(namestr)
-            #names.append(driver.find_element_by_css_selector('.float_lang_base_1.inlineblock').text)
-            print(names[-1])
-            login = WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, 'login')))
-            login.click()
-            email = driver.find_element_by_id('loginFormUser_email')
-            email.click()
-            email.send_keys('964108463@qq.com')
-            password = driver.find_element_by_id('loginForm_password')
-            password.click()
-            password.send_keys('zhang19980531')
-            password.send_keys(Keys.ENTER)
-            sleep(15)
-            WebDriverWait(driver, 60, 0.5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.newBtn.LightGray.downloadBlueIcon.js-download-data'))).click()
-            sleep(10)
-            filename = max([f for f in os.listdir(path)], key=os.path.getctime)
-            if filename[-3:] == 'csv' and (filename not in fileset):
-                os.system('mv "%s/%s" %s/%s' % (path, filename, current_time, key))
-                fileset.add(filename)
-            else:
+        attempts = 0
+        fail = True
+        while attempts < 5 and fail:
+            try:
+                options = webdriver.ChromeOptions()
+                options.add_argument('--headless')
+                options.add_argument('--disable-gpu')
+                options.add_argument('--no-sandbox')
+                desired_capabilities = DesiredCapabilities.CHROME
+                desired_capabilities['pageLoadStrategy'] = 'none'
+                driver = webdriver.Chrome(options=options)
+                driver.get(url)
+                name = (By.CSS_SELECTOR, '.float_lang_base_1.inlineblock')
+                name = WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located(name))
+                namestr = name.text.replace(' ', '_').replace('/', '_')
+                names.append(namestr)
+                #names.append(driver.find_element_by_css_selector('.float_lang_base_1.inlineblock').text)
+                print(names[-1])
+                login = WebDriverWait(driver, 30, 0.5).until(EC.presence_of_element_located((By.CLASS_NAME, 'login')))
+                login.click()
+                email = driver.find_element_by_id('loginFormUser_email')
+                email.click()
+                email.send_keys('964108463@qq.com')
+                password = driver.find_element_by_id('loginForm_password')
+                password.click()
+                password.send_keys('zhang19980531')
+                password.send_keys(Keys.ENTER)
+                sleep(15)
+                WebDriverWait(driver, 60, 0.5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.newBtn.LightGray.downloadBlueIcon.js-download-data'))).click()
+                sleep(10)
+                filename = max([f for f in os.listdir(path)], key=os.path.getctime)
+                if filename[-3:] == 'csv' and (filename not in fileset):
+                    os.system('mv "%s/%s" %s/%s' % (path, filename, current_time, key))
+                    fileset.add(filename)
+                    attempts += 1
+                    fail = False
+                else:
+                    print('Download failure: %s' % url)
+                    attempts += 1
+                driver.quit()
+            except Exception as e:
                 print('Download failure: %s' % url)
-            driver.quit()
-        except Exception as e:
-            print('Download failure: %s' % url)
-            driver.quit()
+                attempts += 1
+                driver.quit()
     country2names[key] = names
 
